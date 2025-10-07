28d37bd4674fe9130d6073bde1683ac1f372bbea
afrozefathima2005-droid <afrozefathima2005@gmail.com>
2025-10-07 11:39:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
+import folium
 
 app = Flask(__name__)
 
@@ -8,12 +9,35 @@ def home():
     # Load the dataset
     data = pd.read_csv('bus_routes.csv')
 
-    # Some simple calculations
+    # Some quick calculations
     total_buses = len(data)
     avg_students = round(data['Avg_Students'].mean(), 2)
     earliest_bus = data.loc[data['Departure_Time'].idxmin()]
 
-    # Pass data to HTML
+    # Create a map centered roughly on the campus
+    m = folium.Map(location=[13.1147, 80.0956], zoom_start=12)
+
+    # Plot each route on the map
+    for _, row in data.iterrows():
+        start = [row['Start_Latitude'], row['Start_Longitude']]
+        end = [row['End_Latitude'], row['End_Longitude']]
+
+        folium.Marker(
+            start, popup=f"Bus {row['Bus_No']} ({row['Start_Stop']}) - {row['Departure_Time']}", 
+            icon=folium.Icon(color='blue', icon='bus', prefix='fa')
+        ).add_to(m)
+
+        folium.Marker(
+            end, popup=f"{row['End_Stop']} (Arrives: {row['Arrival_Time']})", 
+            icon=folium.Icon(color='green')
+        ).add_to(m)
+
+        folium.PolyLine([start, end], color='red', weight=2.5, opacity=1).add_to(m)
+
+    # Save map as HTML file
+    m.save('templates/map.html')
+
+    # Pass data to the main page
     return render_template(
         'index.html',
         tables=data.to_html(classes='data', index=False),
@@ -23,5 +47,10 @@ def home():
         earliest_time=earliest_bus['Departure_Time']
     )
 
+@app.route('/map')
+def map_page():
+    return render_template('map.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
